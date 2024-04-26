49cff5491e6a6de03ce76ae77afbfcf447738e60
charlesperrillat <ce.perrillat@gmail.com>
2024-04-26 19:14:05 +0200

    uploading the program on GitHub

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2_Dictionnaires;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char, string> dictionary = new Dictionary<char, string>();
+
+        dictionary.Add('a', "Abraham");
+        dictionary.Add('b', "Bertrand");
+        dictionary.Add('c', "Charly");
+
+        dictionary.Remove('c');
+
+        foreach (var word in dictionary)
+            Console.WriteLine($"Key: {word.Key}, Value: {word.Value}");
+
+    }
+}
+
